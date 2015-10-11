a05bf814e4997dbe26e704e0e3d92be94f1be786
mpalourdio <mpalourdio@gmail.com>
2015-10-11 16:01:59 +0200

    Add Repository annotation

--- src/main/java/com/mpalourdio/hello/model/TaskRepository.java
+++ src/main/java/com/mpalourdio/hello/model/TaskRepository.java
@@ -3,7 +3,9 @@ package com.mpalourdio.hello.model;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends CrudRepository<Task, Integer> {
     List<Task> findByTaskArchived(int taskArchivedFalse);
 
