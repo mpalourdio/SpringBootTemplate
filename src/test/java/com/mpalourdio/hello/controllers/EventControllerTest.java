d7267d97ac36b8ea0aed9fcc6e64682066935d11
mpalourdio <mpalourdio@gmail.com>
2016-10-29 23:11:36 +0200

    Add some tests example

---
+++ src/test/java/com/mpalourdio/hello/controllers/EventControllerTest.java
@@ -0,0 +1,34 @@
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.mpalourdio.hello.controllers;
+
+import com.mpalourdio.hello.AbstractTestRunner;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EventControllerTest extends AbstractTestRunner {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testEventIsPublished() throws Exception {
+        mvc.perform(get("/event"))
+                .andExpect(content().string("\uD83D\uDE0A -> now I have been published"));
+    }
+}
