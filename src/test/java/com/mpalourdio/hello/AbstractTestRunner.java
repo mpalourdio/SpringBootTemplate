d868daf1f26f22b86d93a82c53717a437a9f3999
mpalourdio <mpalourdio@gmail.com>
2016-10-29 23:11:16 +0200

    Make class abstract

--- src/test/java/com/mpalourdio/hello/AbstractTestRunner.java
+++ src/test/java/com/mpalourdio/hello/AbstractTestRunner.java
@@ -10,11 +10,9 @@
 package com.mpalourdio.hello;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
-public class AbstractTestRunner {
+public abstract class AbstractTestRunner {
 
 }
