ee17ac3c2b26f5332ef5aa15db673674e9b4ee58
mpalourdio <mpalourdio@gmail.com>
2016-10-29 23:16:26 +0200

    Use WebEnvironment.MOCK

--- src/test/java/com/mpalourdio/hello/controllers/EventControllerTest.java
+++ src/test/java/com/mpalourdio/hello/controllers/EventControllerTest.java
@@ -14,12 +14,13 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 public class EventControllerTest extends AbstractTestRunner {
 
