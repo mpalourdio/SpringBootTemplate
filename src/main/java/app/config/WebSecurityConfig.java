6ae2065fd20414456ff8f6271ec4f4be6b3b3332
mpalourdio <mpalourdio@gmail.com>
2016-11-03 21:20:45 +0100

    Add logout

--- src/main/java/app/config/WebSecurityConfig.java
+++ src/main/java/app/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -12,6 +13,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.headers().defaultsDisabled().cacheControl();
+
+        http.logout()
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
     }
 }
 
