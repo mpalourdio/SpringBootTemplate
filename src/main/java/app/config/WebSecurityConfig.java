09b3e4919c7f62e754a6005467f4e2f4537b0d35
mpalourdio <mpalourdio@gmail.com>
2016-11-07 21:04:17 +0100

    Add a LogoutSuccessHandler to show usage of a RedirectStrategy that's not context-relative

--- src/main/java/app/config/WebSecurityConfig.java
+++ src/main/java/app/config/WebSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -17,7 +20,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout()
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
+                .logoutSuccessHandler(logoutHandler())
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+
+    private LogoutSuccessHandler logoutHandler() {
+        final SimpleUrlLogoutSuccessHandler logoutSuccessHandler = new SimpleUrlLogoutSuccessHandler();
+        final DefaultRedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+        redirectStrategy.setContextRelative(false);
+        logoutSuccessHandler.setRedirectStrategy(redirectStrategy);
+
+        return logoutSuccessHandler;
     }
 }
 
