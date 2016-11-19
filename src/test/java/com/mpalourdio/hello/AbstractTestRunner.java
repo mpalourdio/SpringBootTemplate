3240638f6aab436610a81bf23752cf5373210496
mpalourdio <mpalourdio@gmail.com>
2016-11-19 21:32:31 +0100

    Add specific tests properties configuration

--- src/test/java/com/mpalourdio/hello/AbstractTestRunner.java
+++ src/test/java/com/mpalourdio/hello/AbstractTestRunner.java
@@ -10,9 +10,11 @@
 package com.mpalourdio.hello;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:test.properties")
 public abstract class AbstractTestRunner {
 
 }
