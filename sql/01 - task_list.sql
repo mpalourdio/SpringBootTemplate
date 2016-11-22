6c940ef4952b31ef1c2030907252dc40e7188b59
mpalourdio <mpalourdio@gmail.com>
2016-11-22 22:53:07 +0100

    Add sql scripts

---
+++ sql/01 - task_list.sql
@@ -0,0 +1,7 @@
+INSERT INTO task_list (task_id, task_name, task_description, task_priority, task_status, start_date, task_archived)
+VALUES
+  (1, 'Gathering Requirement', 'Requirement Gathering', 'MEDIUM', 'ACTIVE', '2015-10-11 21:01:19', 0),
+  (2, 'Application Designing', 'Application Designing', 'MEDIUM', 'ACTIVE', '2015-10-11 21:01:19', 0),
+  (3, 'Implementation', 'Implementation', 'MEDIUM', 'ACTIVE', '2015-10-11 21:01:19', 0),
+  (4, 'Unit Testing', 'Unit Testing', 'LOW', 'ACTIVE', '2015-10-11 21:01:19', 1),
+  (5, 'Maintanence', 'Maintanence', 'LOW', 'ACTIVE', '2015-10-11 21:01:19', 0);
---
+++ sql/02 - task_people.sql
@@ -0,0 +1,3 @@
+INSERT INTO `task_people` (`people_id`, `name`, `task_id`) VALUES
+(6, 'john', 1),
+(7, 'emma', 2);
