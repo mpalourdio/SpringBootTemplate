4e75c07e4d5bcdb0ab4b0517303cbdcdab91d26a
Michel Palourdio <mpalourdio@gmail.com>
2017-01-08 16:28:52 +0100

    Remove tomcat support

--- src/main/java/com/mpalourdio/springboottemplate/Application.java
+++ src/main/java/com/mpalourdio/springboottemplate/Application.java
@@ -3,8 +3,6 @@ package com.mpalourdio.springboottemplate;
 import app.config.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
@@ -17,7 +15,7 @@ import java.util.Arrays;
         "file:properties/local.properties"
 }, ignoreResourceNotFound = true)
 @Import({WebSecurityConfig.class})
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(final String[] args) {
         final ApplicationContext ctx = SpringApplication.run(Application.class, args);
@@ -26,10 +24,5 @@ public class Application extends SpringBootServletInitializer {
         Arrays.stream(beanNames)
                 .sorted()
                 .forEach(System.out::println);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-        return application.sources(Application.class);
     }
 }
