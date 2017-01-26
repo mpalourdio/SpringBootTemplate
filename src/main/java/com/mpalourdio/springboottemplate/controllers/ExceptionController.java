8bfa42e41b7b64f7f173436005858948cf4c1ce3
Michel Palourdio <mpalourdio@gmail.com>
2017-01-26 22:57:52 +0100

    Add an example on how to propagate error response body and status code from a RestTemplate call

--- src/main/java/com/mpalourdio/springboottemplate/controllers/ExceptionController.java
+++ src/main/java/com/mpalourdio/springboottemplate/controllers/ExceptionController.java
@@ -1,10 +1,15 @@
 package com.mpalourdio.springboottemplate.controllers;
 
 import com.mpalourdio.springboottemplate.exception.CustomException;
-import org.springframework.http.MediaType;
+import com.mpalourdio.springboottemplate.exception.ResponseObject;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/exception")
@@ -17,5 +22,26 @@ public class ExceptionController {
         }
 
         return "all is ok";
+    }
+
+    @GetMapping(value = "/call")
+    public ResponseEntity callException() throws CustomException, IOException {
+        final RestTemplate restTemplate = new RestTemplate();
+        final ResponseEntity<ResponseObject> exchange;
+
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        final HttpEntity httpEntity = new HttpEntity<>(httpHeaders);
+        try {
+            exchange = restTemplate.exchange(
+                    "http://localhost:8080/exception",
+                    HttpMethod.GET,
+                    httpEntity,
+                    ResponseObject.class
+            );
+        } catch (final HttpClientErrorException e) {
+            return ResponseEntity.status(e.getRawStatusCode()).body(e.getResponseBodyAsString());
+        }
+
+        return exchange;
     }
 }
