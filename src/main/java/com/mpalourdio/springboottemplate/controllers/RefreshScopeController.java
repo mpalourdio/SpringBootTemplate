cdec70b7dab0b7d854079aa9cff0475ffc74b481
Michel Palourdio <mpalourdio@gmail.com>
2017-01-29 10:20:26 +0100

    Add a small example on how to refresh a class that fetches configuration value from the configuration server

---
+++ src/main/java/com/mpalourdio/springboottemplate/controllers/RefreshScopeController.java
@@ -0,0 +1,31 @@
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.mpalourdio.springboottemplate.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class RefreshScopeController {
+
+    private final String refreshable;
+
+    public RefreshScopeController(@Value("${refreshable}") final String refreshable) {
+        this.refreshable = refreshable;
+    }
+
+    @GetMapping("/refreshscope")
+    public String refreshScope() {
+        return refreshable;
+    }
+}
