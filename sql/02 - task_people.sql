e9a492ccdadc3199d7714048b36fb6a302b57139
Michel Palourdio <mpalourdio@gmail.com>
2017-03-19 10:09:20 +0100

    Remove MySQL in favor of PostgresSQL

--- sql/02 - task_people.sql
+++ sql/02 - task_people.sql
@@ -1,3 +1,3 @@
-INSERT INTO `task_people` (`people_id`, `name`, `task_id`) VALUES
-(6, 'john', 1),
-(7, 'emma', 2);
+INSERT INTO task_people (people_id, name, task_id) VALUES
+  (6, 'john', 1),
+  (7, 'emma', 2);
