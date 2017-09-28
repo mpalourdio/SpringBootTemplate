8e4d7d47fe67c1e7d8ff76625163383114a5e64a
Michel Palourdio <mpalourdio@gmail.com>
2017-09-28 21:30:25 +0200

    Jackson + enum

---
+++ src/main/java/com/mpalourdio/springboottemplate/model/MyEnum.java
@@ -0,0 +1,30 @@
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.mpalourdio.springboottemplate.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum MyEnum {
+
+    TOTO("tata");
+
+    private final String value;
+
+    private MyEnum(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/com/mpalourdio/springboottemplate/service/EnumableTest.java
@@ -0,0 +1,40 @@
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.mpalourdio.springboottemplate.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mpalourdio.springboottemplate.model.MyEnum;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class EnumableTest {
+
+
+    @Test
+    public void testCanSerializeWithEnums() throws JsonProcessingException {
+        Enumable enumable = new Enumable();
+        enumable.myEnum = MyEnum.TOTO;
+
+        Assert.assertEquals(
+                "{\"myEnum\":\"tata\"}",
+                new ObjectMapper().writeValueAsString(enumable)
+        );
+    }
+
+    @Test
+    public void testCanUnserializeWithEnums() throws IOException {
+        Enumable enumable = new ObjectMapper().readValue("{\"myEnum\":\"tata\"}", Enumable.class);
+
+        Assert.assertEquals(MyEnum.TOTO, enumable.myEnum);
+    }
+}
