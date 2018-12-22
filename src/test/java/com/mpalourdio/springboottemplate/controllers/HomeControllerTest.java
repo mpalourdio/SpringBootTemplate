e3a852e1ca2d798d905fc0ae90432aab2a3d8388
Michel Palourdio <mpalourdio@gmail.com>
2018-12-22 10:17:05 +0100

    Re-add with(csrf()) for test

--- src/test/java/com/mpalourdio/springboottemplate/controllers/HomeControllerTest.java
+++ src/test/java/com/mpalourdio/springboottemplate/controllers/HomeControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -59,6 +60,6 @@ public class HomeControllerTest extends AbstractTestRunner {
 
     @Test
     public void testCanPutWithCsrf() throws Exception {
-        mvc.perform(put("/putput")).andExpect(status().isOk());
+        mvc.perform(put("/putput").with(csrf())).andExpect(status().isOk());
     }
 }
