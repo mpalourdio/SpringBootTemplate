985d01cb86940530e6c4d74008f35e6c653e1be0
Michel Palourdio <mpalourdio@gmail.com>
2019-05-18 12:29:01 +0200

    Return valid JSON

--- src/main/java/com/mpalourdio/springboottemplate/controllers/HttpVersionedApiController.java
+++ src/main/java/com/mpalourdio/springboottemplate/controllers/HttpVersionedApiController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Running a GET request here with ACCEPT header = application/vnd.api.v1+json;q=0.9, application/json
  * will receive response from processV2.
@@ -32,13 +35,13 @@ public class HttpVersionedApiController {
 
 
     @GetMapping(value = "/test", produces = MediaType.APPLICATION_VND_API_V1_VALUE)
-    public String processV1() {
-        return "v1";
+    public List<String> processV1() {
+        return Collections.singletonList("v1");
     }
 
     @GetMapping(value = "/test", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_VND_API_V2_VALUE})
-    public String processV2() {
-        return "v2";
+    public List<String> processV2() {
+        return Collections.singletonList("v2");
     }
 
     @PostMapping(
@@ -46,8 +49,8 @@ public class HttpVersionedApiController {
             consumes = MediaType.APPLICATION_VND_API_V1_VALUE,
             produces = MediaType.APPLICATION_VND_API_V1_VALUE
     )
-    public String processV1Post() {
-        return "v1-post";
+    public List<String> processV1Post() {
+        return Collections.singletonList("v1-post");
     }
 
     @PostMapping(
@@ -55,13 +58,13 @@ public class HttpVersionedApiController {
             consumes = MediaType.APPLICATION_VND_API_V2_VALUE,
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_VND_API_V2_VALUE}
     )
-    public String processV2Post() {
-        return "v2-post";
+    public List<String> processV2Post() {
+        return Collections.singletonList("v2-post");
     }
 
     @PostMapping(value = "/test", consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_VND_API_V1_VALUE)
-    public String processV1PostConsumeAll() {
-        return "v1-post-consumes-all";
+    public List<String> processV1PostConsumeAll() {
+        return Collections.singletonList("v1-post-consumes-all");
     }
 
     @PostMapping(
@@ -69,8 +72,8 @@ public class HttpVersionedApiController {
             consumes = MediaType.ALL_VALUE,
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_VND_API_V2_VALUE}
     )
-    public String processV2PostConsumeAll() {
-        return "v2-post-consumes-all";
+    public List<String> processV2PostConsumeAll() {
+        return Collections.singletonList("v2-post-consumes-all");
     }
 }
 
