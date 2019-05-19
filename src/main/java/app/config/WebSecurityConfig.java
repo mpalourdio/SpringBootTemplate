9e255d15b77cce52a4b5021a991d26ddc49e196a
Michel Palourdio <mpalourdio@gmail.com>
2019-05-19 10:13:47 +0200

    @EnableWebSecurity not needed anymore ?

--- src/main/java/app/config/WebSecurityConfig.java
+++ src/main/java/app/config/WebSecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointR
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -24,7 +23,6 @@ import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuc
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String BASIC_AUTH_ENDPOINT = "/basicauth";
