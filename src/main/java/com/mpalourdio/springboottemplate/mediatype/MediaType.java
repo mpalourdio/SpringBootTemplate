3896a49ee047b1cd8c6aca418e48640f6afda18c
Michel Palourdio <mpalourdio@gmail.com>
2019-05-20 19:54:02 +0200

    Not using "valueOf' to avoid static init cost

--- src/main/java/com/mpalourdio/springboottemplate/mediatype/MediaType.java
+++ src/main/java/com/mpalourdio/springboottemplate/mediatype/MediaType.java
@@ -9,20 +9,22 @@
 
 package com.mpalourdio.springboottemplate.mediatype;
 
-public class MediaType extends org.springframework.http.MediaType {
+import java.nio.charset.StandardCharsets;
+
+public final class MediaType extends org.springframework.http.MediaType {
 
     public static final org.springframework.http.MediaType APPLICATION_VND_API_V1;
-    public static final String APPLICATION_VND_API_V1_VALUE = "application/vnd.api.v1+json";
+    public static final String APPLICATION_VND_API_V1_VALUE = "application/vnd.api.v1+json;charset=UTF-8";
 
     public static final org.springframework.http.MediaType APPLICATION_VND_API_V2;
-    public static final String APPLICATION_VND_API_V2_VALUE = "application/vnd.api.v2+json";
+    public static final String APPLICATION_VND_API_V2_VALUE = "application/vnd.api.v2+json;charset=UTF-8";
 
     private MediaType(String type) {
         super(type);
     }
 
     static {
-        APPLICATION_VND_API_V1 = valueOf(APPLICATION_VND_API_V1_VALUE);
-        APPLICATION_VND_API_V2 = valueOf(APPLICATION_VND_API_V2_VALUE);
+        APPLICATION_VND_API_V1 = new org.springframework.http.MediaType("application", "vnd.api.v1+json", StandardCharsets.UTF_8);
+        APPLICATION_VND_API_V2 = new org.springframework.http.MediaType("application", "vnd.api.v2+json", StandardCharsets.UTF_8);
     }
 }
