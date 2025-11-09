b40226e119358c8184d35aeac85d3f3bcbdb7529
Michel Palourdio <mpalourdio@gmail.com>
2025-11-09 15:27:46 +0100

    chore: Remove unneeded @EnableWebSecurity

--- src/main/java/app/config/WebSecurityConfig.java
+++ src/main/java/app/config/WebSecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.DefaultRedirectStrategy;
@@ -27,7 +26,6 @@ import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuc
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
 @Configuration(proxyBeanMethods = false)
-@EnableWebSecurity
 @EnableConfigurationProperties(CredentialsProperties.class)
 @ConditionalOnWebApplication
 public class WebSecurityConfig {
