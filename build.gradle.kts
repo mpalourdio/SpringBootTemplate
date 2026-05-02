dd6e92ecfbd616d1a8d1c33fbff2191d236c2e52
Michel Palourdio <mpalourdio@gmail.com>
2026-05-02 16:44:43 +0200

    chore(deps): replace spring-boot-starter-web with spring-boot-starter-webmvc

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ checkstyle {
 tasks.checkstyleTest { enabled = false }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-restclient")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
